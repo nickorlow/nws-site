2b4a0fdef69d68cd2118769ca2b2d8b30672813e
Nicholas Orlowsky <nickorlow@nickorlow.com>
2022-12-05 00:20:17 +0000

    removed uncommitted and unready parts of site

--- src/index.tsx
+++ src/index.tsx
@@ -12,18 +12,18 @@ import UptimeCard from "./components/UptimeCard";
 import Footer from "./components/Footer";
 import {Nav, Navbar, NavbarBrand, NavDropdown} from "react-bootstrap";
 import NWSLogo from "./static/images/NWS_Logo.png";
-import Blogs from "./components/Blogs";
+//import Blogs from "./components/Blogs";
 //import NotFoundPage from "./components/NotFoundPage";
-import LoginPage from "./components/LoginPage";
-import RegisterPage from "./components/RegisterPage";
-import VerifyPage from "./components/VerifyPage";
-import DashboardPage from "./components/DashboardPage";
-import CreateCruisePage from "./components/CreateCruisePage";
+//import LoginPage from "./components/LoginPage";
+//import RegisterPage from "./components/RegisterPage";
+//import VerifyPage from "./components/VerifyPage";
+//import DashboardPage from "./components/DashboardPage";
+//import CreateCruisePage from "./components/CreateCruisePage";
 
 function Layout (props: {children: any}) {
     return (
         <div>
-            <Navbar sticky={"top"} style={{backgroundColor: "#eee", paddingLeft: 100, paddingRight: 100}} className={"row"}>
+            {/* <Navbar sticky={"top"} style={{backgroundColor: "#eee", paddingLeft: 100, paddingRight: 100}} className={"row"}>
                 <div className={"col-10"}>
                     <NavbarBrand>
                         <img src={NWSLogo} alt="nws-logo" style={{width: 120}}/>
@@ -35,9 +35,6 @@ function Layout (props: {children: any}) {
                         Status
                     </NavLink>
                 </div>
-                {/*<NavLink className={"nav-lnk"} to={"/blogs"}>*/}
-                {/*    Blog*/}
-                {/*</NavLink>*/}
                 <div className={"col-2"}>
                     { localStorage.getItem("session_key") === null &&
                         (
@@ -61,7 +58,7 @@ function Layout (props: {children: any}) {
                         )
                     }
                 </div>
-            </Navbar>
+            </Navbar> */}
             {props.children}
             <Footer/>
         </div>
@@ -81,55 +78,6 @@ const router = createBrowserRouter([
         element:
             <Layout>
                 <StatusPage/>
-            </Layout>
-    },
-    {
-        path: "blog",
-        element:
-            <Layout>
-                <Blogs/>
-            </Layout>
-    },
-    {
-        path: "blogs",
-        element:
-            <Layout>
-                <Blogs/>
-            </Layout>
-    },
-    {
-        path: "login",
-        element:
-            <Layout>
-                <LoginPage/>
-            </Layout>
-    },
-    {
-        path: "verify",
-        element:
-            <Layout>
-                <VerifyPage/>
-            </Layout>
-    },
-    {
-        path: "dashboard",
-        element:
-            <Layout>
-                <DashboardPage/>
-            </Layout>
-    },
-    {
-        path: "register",
-        element:
-            <Layout>
-                <RegisterPage/>
-            </Layout>
-    },
-    {
-        path: "cruise/new",
-        element:
-            <Layout>
-                <CreateCruisePage/>
             </Layout>
     },
     {
