1ab20739cc51aa8d90b122bf021b6d9c313eff86
Nicholas Orlowsky <nickorlow@nickorlow.com>
2023-01-23 20:51:30 -0600

    some mobile fixes

--- src/components/UptimeComparisonCard.tsx
+++ src/components/UptimeComparisonCard.tsx
@@ -32,20 +32,20 @@ function getResponseTimeClass(respTime: number) {
 export default function UptimeComparisonCard(props: {uptime: UptimeRecord, isService: boolean}) {
     const [isModalOpen, setModalOpen] = useState(false);
     return(
-        <div className={"col-12 col-md-2 mb-2 p-0 m-0 text-center"}>
-                <div style={{height: 25, margin: 0}}>
+        <div className={"col-12 col-md-2 mb-2 p-0 m-0 text-center nws-card-md"}>
+                <div style={{height: 25, margin: 0}} className={"pt-2 pt-md-0"}>
                    <h4 className={"uptime-lnk"} onClick={()=>setModalOpen(true)}>{props.uptime.name}</h4>
                 </div>
                 <hr/>
                 <p className={"fw-bold d-md-none"}>Uptime (Last Month)</p>
                 <p className={getUptimeClass(props.uptime.uptimeMonth)}>{props.uptime.uptimeMonth}%</p>
-                <hr/>
+                <hr className={"d-md-block d-none"}/>
                 <p className={"fw-bold d-md-none"}>Uptime ({new Date().getFullYear()} YTD)</p>
                 <p  className={getUptimeClass(props.uptime.uptimeYtd)}>{props.uptime.uptimeYtd}%</p>
-                <hr/>
+                <hr className={"d-md-block d-none"}/>
                 <p className={"fw-bold d-md-none"}>Avg Response Time (24hr)</p>
                 <p  className={getResponseTimeClass(props.uptime.averageResponseTime)}>{props.uptime.averageResponseTime}ms</p>
-                <hr/>
+                <hr className={"d-md-block d-none"}/>
                 <p className={"fw-bold d-md-none"}>Current Status</p>
                 <div className={`p-1 d-flex justify-content-start w-100`}>
                     <p className={`fw-bold severity-label w-100
